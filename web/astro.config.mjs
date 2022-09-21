24f3da750ce7ad0eaeb9de75d313650b6c96cf8e
Lucas Bernalte <lucas.bernalte@gmail.com>
2022-09-21 12:02:53 +0200

    feat: change working dir

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lucbpz.github.io/second-site',
-    base: '/',
+    site: 'https://lucbpz.github.io',
+    base: '/second-site',
 });
